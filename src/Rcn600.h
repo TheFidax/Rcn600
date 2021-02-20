aa85ff2b271910ac53efb3958fb833699fb8cee0
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-02-20 18:55:52 +0100

    FN_BIT

--- src/Rcn600.h
+++ src/Rcn600.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "Arduino.h"
+#include "SUSI_FN_BIT.h"
 
 //#define	DEBUG_RCN_LIBRARY		//attiva la modalita' di debug
 
@@ -28,84 +29,6 @@ typedef enum {
 	SUSI_FN_53_60,
 	SUSI_FN_61_68,
 } SUSI_FN_GROUP;
-
-#define SUSI_FN_BIT_00	0x10
-#define SUSI_FN_BIT_01	0x01
-#define SUSI_FN_BIT_02	0x02
-#define SUSI_FN_BIT_03	0x04
-#define SUSI_FN_BIT_04	0x08
-
-#define SUSI_FN_BIT_05	0x01
-#define SUSI_FN_BIT_06	0x02
-#define SUSI_FN_BIT_07	0x04
-#define SUSI_FN_BIT_08	0x08
-#define SUSI_FN_BIT_09	0x10
-#define SUSI_FN_BIT_10	0x20
-#define SUSI_FN_BIT_11	0x40
-#define SUSI_FN_BIT_12	0x80
-
-#define SUSI_FN_BIT_13	0x01
-#define SUSI_FN_BIT_14	0x02
-#define SUSI_FN_BIT_15	0x04
-#define SUSI_FN_BIT_16	0x08
-#define SUSI_FN_BIT_17	0x10
-#define SUSI_FN_BIT_18	0x20
-#define SUSI_FN_BIT_19	0x40
-#define SUSI_FN_BIT_20	0x80
-
-#define SUSI_FN_BIT_21	0x01
-#define SUSI_FN_BIT_22	0x02
-#define SUSI_FN_BIT_23	0x04
-#define SUSI_FN_BIT_24	0x08
-#define SUSI_FN_BIT_25	0x10
-#define SUSI_FN_BIT_26	0x20
-#define SUSI_FN_BIT_27	0x40
-#define SUSI_FN_BIT_28	0x80
-
-#define SUSI_FN_BIT_29	0x01
-#define SUSI_FN_BIT_30	0x02
-#define SUSI_FN_BIT_31	0x04
-#define SUSI_FN_BIT_32	0x08
-#define SUSI_FN_BIT_33	0x10
-#define SUSI_FN_BIT_34	0x20
-#define SUSI_FN_BIT_35	0x40
-#define SUSI_FN_BIT_36	0x80
-
-#define SUSI_FN_BIT_37	0x01
-#define SUSI_FN_BIT_38	0x02
-#define SUSI_FN_BIT_39	0x04
-#define SUSI_FN_BIT_40	0x08
-#define SUSI_FN_BIT_41	0x10
-#define SUSI_FN_BIT_42	0x20
-#define SUSI_FN_BIT_43	0x40
-#define SUSI_FN_BIT_44	0x80
-
-#define SUSI_FN_BIT_45	0x01
-#define SUSI_FN_BIT_46	0x02
-#define SUSI_FN_BIT_47	0x04
-#define SUSI_FN_BIT_48	0x08
-#define SUSI_FN_BIT_49	0x10
-#define SUSI_FN_BIT_50	0x20
-#define SUSI_FN_BIT_51	0x40
-#define SUSI_FN_BIT_52	0x80
-
-#define SUSI_FN_BIT_53	0x01
-#define SUSI_FN_BIT_54	0x02
-#define SUSI_FN_BIT_55	0x04
-#define SUSI_FN_BIT_56	0x08
-#define SUSI_FN_BIT_57	0x10
-#define SUSI_FN_BIT_58	0x20
-#define SUSI_FN_BIT_59	0x40
-#define SUSI_FN_BIT_60	0x80
-
-#define SUSI_FN_BIT_61	0x01
-#define SUSI_FN_BIT_62	0x02
-#define SUSI_FN_BIT_63	0x04
-#define SUSI_FN_BIT_64	0x08
-#define SUSI_FN_BIT_65	0x10
-#define SUSI_FN_BIT_66	0x20
-#define SUSI_FN_BIT_67	0x40
-#define SUSI_FN_BIT_68	0x80
 
 typedef struct {
 	uint8_t		CLK_pin;			//pin a cui è collegata la linea "Clock";		DEVE ESSERE DI TIPO INTERRUPT
---
+++ src/SUSI_FN_BIT.h
@@ -0,0 +1,85 @@
+#ifndef SUSI_FN_BIT_h
+#define SUSI_FN_BIT_h
+
+// bit che identificano lo stato delle funzioni
+
+#define SUSI_FN_BIT_00	0x10
+#define SUSI_FN_BIT_01	0x01
+#define SUSI_FN_BIT_02	0x02
+#define SUSI_FN_BIT_03	0x04
+#define SUSI_FN_BIT_04	0x08
+
+#define SUSI_FN_BIT_05	0x01
+#define SUSI_FN_BIT_06	0x02
+#define SUSI_FN_BIT_07	0x04
+#define SUSI_FN_BIT_08	0x08
+#define SUSI_FN_BIT_09	0x10
+#define SUSI_FN_BIT_10	0x20
+#define SUSI_FN_BIT_11	0x40
+#define SUSI_FN_BIT_12	0x80
+
+#define SUSI_FN_BIT_13	0x01
+#define SUSI_FN_BIT_14	0x02
+#define SUSI_FN_BIT_15	0x04
+#define SUSI_FN_BIT_16	0x08
+#define SUSI_FN_BIT_17	0x10
+#define SUSI_FN_BIT_18	0x20
+#define SUSI_FN_BIT_19	0x40
+#define SUSI_FN_BIT_20	0x80
+
+#define SUSI_FN_BIT_21	0x01
+#define SUSI_FN_BIT_22	0x02
+#define SUSI_FN_BIT_23	0x04
+#define SUSI_FN_BIT_24	0x08
+#define SUSI_FN_BIT_25	0x10
+#define SUSI_FN_BIT_26	0x20
+#define SUSI_FN_BIT_27	0x40
+#define SUSI_FN_BIT_28	0x80
+
+#define SUSI_FN_BIT_29	0x01
+#define SUSI_FN_BIT_30	0x02
+#define SUSI_FN_BIT_31	0x04
+#define SUSI_FN_BIT_32	0x08
+#define SUSI_FN_BIT_33	0x10
+#define SUSI_FN_BIT_34	0x20
+#define SUSI_FN_BIT_35	0x40
+#define SUSI_FN_BIT_36	0x80
+
+#define SUSI_FN_BIT_37	0x01
+#define SUSI_FN_BIT_38	0x02
+#define SUSI_FN_BIT_39	0x04
+#define SUSI_FN_BIT_40	0x08
+#define SUSI_FN_BIT_41	0x10
+#define SUSI_FN_BIT_42	0x20
+#define SUSI_FN_BIT_43	0x40
+#define SUSI_FN_BIT_44	0x80
+
+#define SUSI_FN_BIT_45	0x01
+#define SUSI_FN_BIT_46	0x02
+#define SUSI_FN_BIT_47	0x04
+#define SUSI_FN_BIT_48	0x08
+#define SUSI_FN_BIT_49	0x10
+#define SUSI_FN_BIT_50	0x20
+#define SUSI_FN_BIT_51	0x40
+#define SUSI_FN_BIT_52	0x80
+
+#define SUSI_FN_BIT_53	0x01
+#define SUSI_FN_BIT_54	0x02
+#define SUSI_FN_BIT_55	0x04
+#define SUSI_FN_BIT_56	0x08
+#define SUSI_FN_BIT_57	0x10
+#define SUSI_FN_BIT_58	0x20
+#define SUSI_FN_BIT_59	0x40
+#define SUSI_FN_BIT_60	0x80
+
+#define SUSI_FN_BIT_61	0x01
+#define SUSI_FN_BIT_62	0x02
+#define SUSI_FN_BIT_63	0x04
+#define SUSI_FN_BIT_64	0x08
+#define SUSI_FN_BIT_65	0x10
+#define SUSI_FN_BIT_66	0x20
+#define SUSI_FN_BIT_67	0x40
+#define SUSI_FN_BIT_68	0x80
+
+
+#endif
