a2a29b9cacec148140d7da45951348fdb936300e
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-02-20 19:29:22 +0100

    Aggiunto esempio + aggiunte parole chiave

---
+++ examples/LedBuiltin_SUSIF0/LedBuiltin_SUSIF0.ino
@@ -0,0 +1,24 @@
+/*
+*	Questo esempio permette di vedere la corretta decodifica dell'interfaccia SUSI accendendo il led incorporato nella scheda quando la Funzione0 e' attiva
+*/
+
+#include <Rcn600.h>      // Includo la libreria per la gestione della SUSI
+
+Rcn600 SUSI(2, 3);      // (CLK pin, DATA pin) il pin di Clock DEVE ESSERE di tipo interrupt, il pin Data puo' essere in pin qualsiasi: compresi gli analogici
+
+void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) {
+  switch( SUSI_FuncGrp ) {
+     case SUSI_FN_0_4:
+       ((SUSI_FuncState & SUSI_FN_BIT_00) ? digitalWrite(LED_BUILTIN, HIGH) : digitalWrite(LED_BUILTIN, LOW));
+       break;
+   }
+}
+
+void setup() {   
+  pinMode(LED_BUILTIN, OUTPUT);
+  SUSI.init();      // Avvio la libreria
+}
+
+void loop() {
+  SUSI.process();     // Elaboro pi� volte possibile i dati acquisiti
+}
