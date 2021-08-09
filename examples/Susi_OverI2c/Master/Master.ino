3265900ed69b15c613f5de4ca13bc14a36c0f912
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-08-09 21:35:21 +0200

    Update Master.ino

--- examples/Susi_OverI2c/Master/Master.ino
+++ examples/Susi_OverI2c/Master/Master.ino
@@ -5,17 +5,17 @@ void setup() {
     Serial.begin(115200);
 
     //inizializzo il Bus I2C per eventuali Moduli esterni
-    initSusiOverI2C(MAIN_DECODER_ADDRESS, &SUSI);
+    initSusiOverI2C(MAIN_DECODER_ADDRESS);
 
     while (SearchExternalI2CDevices() == 0) {}  // Attendo che venga trovato almeno un dispositivo I2c
 }
 
 void loop(void) {
-    sendSusiMessage(0x060, 0b00010000, 0);
+    sendSusiMessage(0x060, 0b00010000, 0);	// Attivo la F0
 
     delay(1000);
 
-    sendSusiMessage(0x60, 0, 0);
+    sendSusiMessage(0x60, 0, 0);		// Disattivo la F0
 
     delay(1000);
 }
