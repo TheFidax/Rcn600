1ff45127151c820bb7d8b968d68786c32f7f62a3
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-08-09 18:49:12 +0200

    Update Rcn600.cpp

--- src/Rcn600.cpp
+++ src/Rcn600.cpp
@@ -85,7 +85,7 @@ void Rcn600::initClass(void) {
 	}
 
 	for (uint8_t i = 0; i < SUSI_BUFFER_LENGTH; ++i) {	// Imposto gli slot del Buffer come liberi
-		_Buffer[i].nextMessage = NULL;
+		_Buffer[i].nextMessage = FREE_MESSAGE_SLOT;
 	}
 
 	_BufferPointer = NULL;								// Imposto il puntatore per indicare l'assenza di messaggi da decodificare
@@ -140,7 +140,7 @@ void Rcn600::init(uint8_t SlaveAddress) {		/* Inizializzazione con indirizzo sce
 
 Rcn600Message* Rcn600::searchFreeMessage(void) {
 	for (uint8_t i = 0; i < SUSI_BUFFER_LENGTH; ++i) {
-		if (_Buffer[i].nextMessage == NULL) {
+		if (_Buffer[i].nextMessage == FREE_MESSAGE_SLOT) {
 			return &_Buffer[i];
 		}
 	}
@@ -190,7 +190,7 @@ int8_t Rcn600::addManualMessage(uint8_t firstByte, uint8_t secondByte, uint8_t C
 
 				process();									// Processo il messaggio acquisito
 
-				_messageSlot->nextMessage = NULL;			// Libero lo Slot per scriverci dentro un nuovo messaggio
+				_messageSlot->nextMessage = FREE_MESSAGE_SLOT;			// Libero lo Slot per scriverci dentro un nuovo messaggio
 				_BufferPointer = original;					// Ripristino la coda di messaggi acquisiti
 			}
 
@@ -251,7 +251,7 @@ void Rcn600::ISR_SUSI(void) {
 
 			/* Controllo se ho letto un Byte (8 bit) */
 			if (_bitCounter == 8 || _bitCounter == 16 || _bitCounter == 24) {
-				/* Se ho letto un Byte, memorizzo il momento in cui la lettura è avvenuta e resetto il contatore dei bit */
+				/* Se ho letto un Byte, memorizzo il momento in cui la lettura Ã¨ avvenuta e resetto il contatore dei bit */
 				_lastByte_time = millis();
 
 				// Se sono qui _bitCounter e' uguale a 8 , 16 o 24
@@ -274,7 +274,7 @@ void Rcn600::ISR_SUSI(void) {
 
 						process();									// Processo il messaggio acquisito
 
-						_messageSlot->nextMessage = NULL;			// Libero lo Slot per scriverci dentro un nuovo messaggio
+						_messageSlot->nextMessage = FREE_MESSAGE_SLOT;			// Libero lo Slot per scriverci dentro un nuovo messaggio
 
 						_BufferPointer = original;					// Ripristino la coda di messaggi acquisiti
 
@@ -448,7 +448,7 @@ void Rcn600::process(void) {
 				break;
 			}
 			case 109: {
-				/* "Binärzustände kurze Form" :  0110 - 1101 (0x6D = 109) D L6 L5 L4 - L3 L2 L1 L0
+				/* "BinÃ¤rzustÃ¤nde kurze Form" :  0110 - 1101 (0x6D = 109) D L6 L5 L4 - L3 L2 L1 L0
 				*
 				*	D = 0 bedeutet Funktion L ausgeschaltet, D = 1 eingeschaltet
 				*	L = Funktionsnummer 1 ... 127
@@ -490,12 +490,12 @@ void Rcn600::process(void) {
 				break;
 			}
 			case 110: {	// && 111
-				/*	"Binärzustände lange Form low Byte" : 0110-1110 (0x6E = 110) D L6 L5 L4 - L3 L2 L1 L0
+				/*	"BinÃ¤rzustÃ¤nde lange Form low Byte" : 0110-1110 (0x6E = 110) D L6 L5 L4 - L3 L2 L1 L0
 				*
-				*	Befehl wird immer paarweise vor dem Binärzustand lange Form high Byte gesendet.
+				*	Befehl wird immer paarweise vor dem BinÃ¤rzustand lange Form high Byte gesendet.
 				*	Folgen die beiden Befehle nicht direkt aufeinander, so sind sie zu ignorieren.
-				*	D = 0 bedeutet Binärzustand L ausgeschaltet, D = 1 "eingeschaltet"
-				*	L = niederwertige Bits der Binärzustandsnummer 1 ... 32767
+				*	D = 0 bedeutet BinÃ¤rzustand L ausgeschaltet, D = 1 "eingeschaltet"
+				*	L = niederwertige Bits der BinÃ¤rzustandsnummer 1 ... 32767
 				*
 				*	Il comando viene sempre inviato in coppie prima che lo stato binario formi lunghi byte alti.
 				*	Se i due comandi non seguono direttamente, devono essere ignorati.
@@ -503,13 +503,13 @@ void Rcn600::process(void) {
 				*	L = bit di basso valore dello stato binario numero 1 ... 32767
 				*
 				*
-				*	"Binärzustände lange Form high Byte" : 0110-1111 (0x6F = 111) H7 H6 H5 H4 - H3 H2 H1 H0
+				*	"BinÃ¤rzustÃ¤nde lange Form high Byte" : 0110-1111 (0x6F = 111) H7 H6 H5 H4 - H3 H2 H1 H0
 				*
-				*	Befehl wird immer paarweise nach dem Binärzustand lange Form low
+				*	Befehl wird immer paarweise nach dem BinÃ¤rzustand lange Form low
 				*	Byte gesendet. Folgen die beiden Befehle nicht direkt aufeinander, so sind sie zu ignorieren.
-				*	Erst dieser Befehl führt zur Ausführung des Gesamtbefehls.
-				*	H = höherwertigen Bits der Binärzustandsnummer high 1 ... 32767
-				*	H und L = 0 (Broadcast) schaltet alle 32767 verfügbaren Binärzustände
+				*	Erst dieser Befehl fÃ¼hrt zur AusfÃ¼hrung des Gesamtbefehls.
+				*	H = hÃ¶herwertigen Bits der BinÃ¤rzustandsnummer high 1 ... 32767
+				*	H und L = 0 (Broadcast) schaltet alle 32767 verfÃ¼gbaren BinÃ¤rzustÃ¤nde
 				*	aus (D = 0) oder an (D = 1)
 				*
 				*	Il comando viene sempre inviato in coppie dopo che lo stato binario forma lunghi byte bassi. Se i due comandi non si sus seguono direttamente, devono essere ignorati.
@@ -543,13 +543,13 @@ void Rcn600::process(void) {
 			case 64: {
 				/*	"Direktbefehl 1" (2-Byte): 0100-0000 (0x40 = 64) X8 X7 X6 X5 - X4 X3 X2 X1
 				*
-				*	Die Direktbefehle dienen zur direkten Ansteuerung von Ausgängen und
+				*	Die Direktbefehle dienen zur direkten Ansteuerung von AusgÃ¤ngen und
 				*	anderen Funktionen nach der Anwendung der Funktionstabelle im Master.
 				*	Ein Bit = 1 bedeutet der entsprechende Ausgang ist eingeschaltet.
 				*
 				*	I comandi diretti vengono utilizzati per il controllo diretto delle uscite e
 				*	altre funzioni dopo aver utilizzato la tabella delle funzioni nel master.
-				*	Un bit = 1 significa che l'uscita corrispondente è attivata.
+				*	Un bit = 1 significa che l'uscita corrispondente Ã¨ attivata.
 				*/
 				if (notifySusiAux) {
 					notifySusiAux(SUSI_AUX_1_8, _BufferPointer->Byte[1]);
@@ -580,8 +580,8 @@ void Rcn600::process(void) {
 			case 33: {
 				/*	"Trigger-Puls" : 0010-0001 (0x21 = 33) 0 0 0 0 - 0 0 0 1
 				*
-				*	Der Befehl dient zur Synchronisation eines Dampfstoßes. Er wird je
-				*	Dampfstoß einmal gesendet. Die Bits 1 bis 7 sind für zukünftige
+				*	Der Befehl dient zur Synchronisation eines DampfstoÃŸes. Er wird je
+				*	DampfstoÃŸ einmal gesendet. Die Bits 1 bis 7 sind fÃ¼r zukÃ¼nftige
 				*	Anwendungen reserviert.
 				*
 				*
@@ -598,9 +598,9 @@ void Rcn600::process(void) {
 				/*	"Strom" : 0010-0011 (0x23 = 35) S7 S6 S5 S4 - S3 S2 S1 S0
 				*
 				*	Vom Motor verbrauchter Strom. Der Wert hat einen Bereich von -128 bis
-				*	127, wird im 2er-Komplement übertragen und wird über eine
+				*	127, wird im 2er-Komplement Ã¼bertragen und wird Ã¼ber eine
 				*	herstellerspezifische CV im Lokdecoder kalibriert. Negative Werte
-				*	bedeuten ein Rückspeisen wie es bei modernen E-Loks möglich ist.
+				*	bedeuten ein RÃ¼ckspeisen wie es bei modernen E-Loks mÃ¶glich ist.
 				*
 				*	Elettricita' consumata dal motore. Il valore ha un intervallo da -128 a
 				*	127, viene trasmesso in complemento a 2 e viene trasmesso tramite a
@@ -617,12 +617,12 @@ void Rcn600::process(void) {
 				*
 				*	Die Fahrstufe und Richtung entsprechen dem realen Zustand am Motor.
 				*	Der Wert G ist als 0...127 auf die im Modell eingestellte Vmax normiert zu
-				*	übertragen. G = 0 bedeutet Lok steht, G = 1 ... 127 ist die normierte
-				*	Geschwindigkeit R = Fahrtrichtung mit R = 0 für rückwärts und R = 1
-				*	für vorwärts.
-				*	Dieser und der folgende Befehl werden nicht für neue Implementierungen
-				*	empfohlen. Slaves sollten nach Möglichkeit die Befehle 0x50 bis 0x52
-				*	auswerten. Master, die aus Gründen der Kompatibilität zu existierenden
+				*	Ã¼bertragen. G = 0 bedeutet Lok steht, G = 1 ... 127 ist die normierte
+				*	Geschwindigkeit R = Fahrtrichtung mit R = 0 fÃ¼r rÃ¼ckwÃ¤rts und R = 1
+				*	fÃ¼r vorwÃ¤rts.
+				*	Dieser und der folgende Befehl werden nicht fÃ¼r neue Implementierungen
+				*	empfohlen. Slaves sollten nach MÃ¶glichkeit die Befehle 0x50 bis 0x52
+				*	auswerten. Master, die aus GrÃ¼nden der KompatibilitÃ¤t zu existierenden
 				*	Produkten abweichende und/oder unterschiedliche Umsetzungen bei den
 				*	Befehlen 0x24 und 0x25 verwenden, sind normkonform.
 				*
@@ -650,8 +650,8 @@ void Rcn600::process(void) {
 				/*	" "Soll" Lok-Fahrstufe " : 0010-0101 (0x25 = 37) R G6 G5 G4 - G3 G2 G1 G0
 				*
 				*	Empfangene Fahrstufe des "Masters" auf 127 Fahrstufen normiert.
-				*	G = 0 bedeutet Lok hält, G = 1 ... 127 ist die normierte Geschwindigkeit
-				*	R = Fahrtrichtung mit R = 0 für rückwärts und R = 1 für vorwärts
+				*	G = 0 bedeutet Lok hÃ¤lt, G = 1 ... 127 ist die normierte Geschwindigkeit
+				*	R = Fahrtrichtung mit R = 0 fÃ¼r rÃ¼ckwÃ¤rts und R = 1 fÃ¼r vorwÃ¤rts
 				*
 				*	Passo di velocita' ricevuto del "master" normalizzato a 127 passi di velocita'.
 				*	G = 0 significa che la locomotiva si ferma, G = 1 ... 127 e' la velocita' normalizzata
@@ -670,9 +670,9 @@ void Rcn600::process(void) {
 			case 38: {
 				/*	"Lastausregelung" : 0010-0110 (0x26 = 38) P7 P6 P5 P4 - P3 P2 P1 P0
 				*
-				*	Die Erfassung des Lastzustandes kann über Motorspannung, -strom oder -
+				*	Die Erfassung des Lastzustandes kann Ã¼ber Motorspannung, -strom oder -
 				*	leistung erfolgen. 0 = keine Last, 127 = maximale Last. Es sind auch
-				*	negative Werte möglich, die im 2er-Komplement übertragen werden.
+				*	negative Werte mÃ¶glich, die im 2er-Komplement Ã¼bertragen werden.
 				*	Diese bedeuten weniger Last als Fahren in der Ebene.
 				*
 				*	Lo stato del carico puo' essere registrato tramite tensione del motore, corrente o prestazione.
@@ -691,11 +691,11 @@ void Rcn600::process(void) {
 				*
 				*	Die Geschwindigkeit und Richtung entsprechen dem realen Zustand am
 				*	Motor. Dieses ist ein Regelwert in Bezug auf die "Soll"- Geschwindigkeit,
-				*	d.h. dass nach Nachführung der Geschwindigkeitsrampe Ist und Soll gleich
+				*	d.h. dass nach NachfÃ¼hrung der Geschwindigkeitsrampe Ist und Soll gleich
 				*	sein sollen (ausgeregelter Zustand). Der Wert G ist als 0...127 auf die im
-				*	Modell eingestellte Vmax normiert zu übertragen. G = 0 bedeutet Lok steht, G
+				*	Modell eingestellte Vmax normiert zu Ã¼bertragen. G = 0 bedeutet Lok steht, G
 				*	= 1 ... 127 ist die normiert Geschwindigkeit R = Fahrtrichtung mit R = 0
-				*	für rückwärts und R = 1 für vorwärts
+				*	fÃ¼r rÃ¼ckwÃ¤rts und R = 1 fÃ¼r vorwÃ¤rts
 				*
 				*	La velocita' e la direzione corrispondono alla situazione reale del Motore.
 				*	Questo e' un valore di controllo in relazione alla velocita' "target",
@@ -717,18 +717,18 @@ void Rcn600::process(void) {
 			case 81: {
 				/*	" "Soll" Geschwindigkeit " : 0101-0001 (0x51 = 81) R G6 G5 G4 - G3 G2 G1 G0
 				*
-				*	Interne Fahrstufe des "Masters" entsprechend der über die Kennlinie (CVs
+				*	Interne Fahrstufe des "Masters" entsprechend der Ã¼ber die Kennlinie (CVs
 				*	67 bis 94, CVs 2, 6 und 5) und sonstiger CVs, die die Geschwindigkeit des
 				*	Fahrzeugs bestimmen, umgerechnete empfangene Fahrstufe auf 127
-				*	Fahrstufen normalisiert. D.h. der höchste aufgrund der CVs 94 und/oder
+				*	Fahrstufen normalisiert. D.h. der hÃ¶chste aufgrund der CVs 94 und/oder
 				*	CV5 bzw. sonstiger entsprechender CVs erreichbare Wert wird auf 127
-				*	normiert. CVs für Beschleunigung und Bremsen wie die CVs 3, 4, 23 und
-				*	24 gehen in die Berechnung nicht ein. G = 0 bedeutet Lok hält, G = 1 ...
-				*	127 ist die Geschwindigkeit R = Fahrtrichtung mit R = 0 für rückwärts und
-				*	R = 1 für vorwärts.
+				*	normiert. CVs fÃ¼r Beschleunigung und Bremsen wie die CVs 3, 4, 23 und
+				*	24 gehen in die Berechnung nicht ein. G = 0 bedeutet Lok hÃ¤lt, G = 1 ...
+				*	127 ist die Geschwindigkeit R = Fahrtrichtung mit R = 0 fÃ¼r rÃ¼ckwÃ¤rts und
+				*	R = 1 fÃ¼r vorwÃ¤rts.
 				*	Da die Decoder unterschiedliche Verfahren zur Bestimmung der
-				*	Höchstgeschwindigkeit verwenden, kann es hier leicht unterschiedliche
-				*	Implementationen geben. Wichtig ist vor allem, dass sich die Befehle für
+				*	HÃ¶chstgeschwindigkeit verwenden, kann es hier leicht unterschiedliche
+				*	Implementationen geben. Wichtig ist vor allem, dass sich die Befehle fÃ¼r
 				*	Ist und Soll-Geschwindigkeit gleich verhalten.
 				*
 				*	Livello di guida interno del "Master" in base al controllo della caratteristica (CV)
@@ -763,7 +763,7 @@ void Rcn600::process(void) {
 			case 40: {
 				/*	"Analogfunktionsgruppe 1" : 0010-1000 (0x28 = 40) A7 A6 A5 A4 - A3 A2 A1 A0
 				*
-				*	Die acht Befehle dieser Gruppe erlauben die Übertragung von acht
+				*	Die acht Befehle dieser Gruppe erlauben die Ãœbertragung von acht
 				*	verschiedenen Analogwerten im Digitalbetrieb.
 				*
 				*	Gli otto comandi di questo gruppo consentono la trasmissione di otto diversi valori analogici in modalita' digitale.
@@ -823,15 +823,15 @@ void Rcn600::process(void) {
 				break;
 			}
 			case 48: {
-				/*	"Direktbefehl 1 für Analogbetrieb" : 0011-0000 (0x30 = 48) D7 D6 D5 D4 - D3 D2 D1 D0
+				/*	"Direktbefehl 1 fÃ¼r Analogbetrieb" : 0011-0000 (0x30 = 48) D7 D6 D5 D4 - D3 D2 D1 D0
 				*
 				*	Einstellung von Grundfunktionen im Analogbetrieb unter Umgehung einer Funktionszuordnung.
 				*	- Bit 0: Sound an/aus
-				*	- Bit 1: Auf-/Abrüsten
+				*	- Bit 1: Auf-/AbrÃ¼sten
 				*	- Bit 2-6: reserviert
-				*	- Bit 7: Reduzierte Lautstärke
+				*	- Bit 7: Reduzierte LautstÃ¤rke
 				*
-				*	Impostazione delle funzioni di base in modalità analogica ignorando un'assegnazione di funzione.
+				*	Impostazione delle funzioni di base in modalitÃ  analogica ignorando un'assegnazione di funzione.
 				*	- Bit 0: Suono on/off
 				*	- Bit 1: Aggiornamento/Disarmo
 				*	- Bit 2-6: riservato
@@ -843,7 +843,7 @@ void Rcn600::process(void) {
 				break;
 			}
 			case 49: {
-				/*	"Direktbefehl 2 für Analogbetrieb" : 0011-0000 (0x31 = 49) D7 D6 D5 D4 - D3 D2 D1 D0
+				/*	"Direktbefehl 2 fÃ¼r Analogbetrieb" : 0011-0000 (0x31 = 49) D7 D6 D5 D4 - D3 D2 D1 D0
 				*
 				*	Einstellung von Grundfunktionen im Analogbetrieb unter Umgehung einer Funktionszuordnung.
 				*	- Bit 0: Spitzenlicht
@@ -851,7 +851,7 @@ void Rcn600::process(void) {
 				*	- Bit 2: Standlicht
 				*	- Bit 3-7: reserviert
 				*
-				*	Impostazione delle funzioni di base in modalità analogica ignorando un'assegnazione di funzione.
+				*	Impostazione delle funzioni di base in modalitÃ  analogica ignorando un'assegnazione di funzione.
 				*	- Bit 0: Luce di picco
 				*	- Bit 1: Fanale posteriore
 				*	- Bit 2: Stand light
@@ -865,8 +865,8 @@ void Rcn600::process(void) {
 			case 0: {
 				/*	"No Operation" : 0000-0000 (0x00 = 0) X X X X - X X X X
 				*
-				*	Der Befehl bewirkt keine Aktion im Slave. Die Daten können einen
-				*	beliebigen Wert haben. Der Befehl kann als Lückenfüller oder zu
+				*	Der Befehl bewirkt keine Aktion im Slave. Die Daten kÃ¶nnen einen
+				*	beliebigen Wert haben. Der Befehl kann als LÃ¼ckenfÃ¼ller oder zu
 				*	Testzwecken verwendet werden.
 				*
 				*	Il comando non esegue alcuna azione nello slave.
@@ -880,7 +880,7 @@ void Rcn600::process(void) {
 			case 94: {	//&& 95
 				/*	"Moduladresse low" : 0101-1110 (0x5E = 94) A7 A6 A5 A4 - A3 A2 A1 A0
 				*
-				*	Übermittelt die niederwertigen Bits der aktiven Digitaladresse des "Masters", wenn er sich in einer digitalen Betriebsart befindet.
+				*	Ãœbermittelt die niederwertigen Bits der aktiven Digitaladresse des "Masters", wenn er sich in einer digitalen Betriebsart befindet.
 				*	Der Befehl wird immer paarweise vor der Adresse high Byte gesendet.
 				*	Folgen die beiden Befehle nicht direkt aufeinander, so sind sie zu ignorieren.
 				*
@@ -891,7 +891,7 @@ void Rcn600::process(void) {
 				*
 				*	"Moduladresse high" : 0101-1111 (0x5F = 95) A15 A14 A13 A12 - A11 A10 A9 A8
 				*
-				*	Übermittelt die höherwertigen Bits der aktiven Digitaladresse des "Masters", wenn er sich in einer digitalen Betriebsart befindet.
+				*	Ãœbermittelt die hÃ¶herwertigen Bits der aktiven Digitaladresse des "Masters", wenn er sich in einer digitalen Betriebsart befindet.
 				*	Der Befehl wird immer paarweise nach der Adresse low Byte gesendet.
 				*	Folgen die beiden Befehle nicht direkt aufeinander, so sind sie zu ignorieren.
 				*
@@ -924,7 +924,7 @@ void Rcn600::process(void) {
 				/*	"Modul-Steuerbyte" : 0110-1100 (0x6C = 108) B7 B6 B5 B4 - B3 B2 B1 B0
 				*
 				*	- Bit 0 = Buffer Control: 0 = Puffer aus, 1 = Puffer an
-				*	- Bit 1 = Rücksetzfunktion: 0 = alle Funktionen auf "Aus" setzen, 1 = normaler Betrieb
+				*	- Bit 1 = RÃ¼cksetzfunktion: 0 = alle Funktionen auf "Aus" setzen, 1 = normaler Betrieb
 				*	Alle weiteren Bits von der RailCommunity reserviert.
 				*	Falls implementiert sind nach einem Reset die Bits 0 und 1 im Slave auf 1 zu setzen.
 				*
@@ -944,11 +944,11 @@ void Rcn600::process(void) {
 					/* METODI MANIPOLAZIONE CVs */
 
 			case 119: {
-				/*	"CV-Manipulation Byte prüfen" : 0111-0111 (0x77 = 119) | 1 V6 V5 V4 - V3 V2 V1 V0 | D7 D6 D5 D4 - D3 D2 D1 D0
+				/*	"CV-Manipulation Byte prÃ¼fen" : 0111-0111 (0x77 = 119) | 1 V6 V5 V4 - V3 V2 V1 V0 | D7 D6 D5 D4 - D3 D2 D1 D0
 				*
-				*	DCC-Befehl Byte Prüfen im Service- und Betriebsmodus
+				*	DCC-Befehl Byte PrÃ¼fen im Service- und Betriebsmodus
 				*	V = CV-Nummer 897 .. 1024 (Wert 0 = CV 897, Wert 127 = CV 1024)
-				*	D = Vergleichswert zum Prüfen. Wenn D dem gespeicherten CV-Wert
+				*	D = Vergleichswert zum PrÃ¼fen. Wenn D dem gespeicherten CV-Wert
 				*	entspricht, antwortet der Slave mit einem Acknowledge.
 				*	Dieser und die beiden folgenden Befehle sind die in Abschnitt 4 genannten
 				*	3 Byte Pakete entsprechend [RCN-214]
@@ -1003,10 +1003,10 @@ void Rcn600::process(void) {
 				*
 				* DCC-Befehl Bit Manipulieren im Service- und Betriebsmodus
 				* V = CV-Nummer 897 ... 1024 (Wert 0 = CV 897, Wert 127 = CV 1024)
-				* K = 0: Bit Prüfen. Wenn D mit dem Bitzustand an der Bitstelle B der CV übereinstimmt,
+				* K = 0: Bit PrÃ¼fen. Wenn D mit dem Bitzustand an der Bitstelle B der CV Ã¼bereinstimmt,
 				* wird mit einem Acknowledge geantwortet.
 				* K = 1: Bit Schreiben. D wird in Bitstelle B der CV geschrieben.
-				* Der Slave bestätigt das Schreiben mit einem Acknowledge.
+				* Der Slave bestÃ¤tigt das Schreiben mit einem Acknowledge.
 				*
 				* Manipolazione dei bit di comando DCC in modalita' di servizio e operativa
 				* V = numero CV 897 ... 1024 (valore 0 = CV 897, valore 127 = CV 1024)
@@ -1077,7 +1077,7 @@ void Rcn600::process(void) {
 				*
 				* DCC-Befehl Byte Schreiben im Service- und Betriebsmodus
 				* V = CV-Nummer 897 .. 1024 (Wert 0 = CV 897, Wert 127 = CV 1024)
-				* D = Wert zum Schreiben in die CV. Der Slave bestätigt das Schreiben mit
+				* D = Wert zum Schreiben in die CV. Der Slave bestÃ¤tigt das Schreiben mit
 				* einem Acknowledge.
 				*
 				* Scrittura byte di comando DCC in modalita' di servizio e operativa
@@ -1126,7 +1126,7 @@ void Rcn600::process(void) {
 
 			// Aggiorno il puntatore del Buffer
 			Rcn600Message* p = _BufferPointer->nextMessage;
-			_BufferPointer->nextMessage = NULL;
+			_BufferPointer->nextMessage = FREE_MESSAGE_SLOT;
 			_BufferPointer = p;
 		}
 	}
