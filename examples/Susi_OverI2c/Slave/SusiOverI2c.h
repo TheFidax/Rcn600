c1fcb83ec808f3b36453135dcc2608f84172a367
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-08-09 21:17:39 +0200

    Update SusiOverI2c.h

--- examples/Susi_OverI2c/Slave/SusiOverI2c.h
+++ examples/Susi_OverI2c/Slave/SusiOverI2c.h
@@ -13,17 +13,6 @@
 void receiveEvent_SusiOverI2C(int bytes);
 void requestEvent_SusiOverI2C(void);
 
-extern uint8_t nDevices;						// indica quanti dispositivi sono collegati al Bus I2c
-
-// Funzioni per il Master del BUS 
-  /*
-  * initSusiOverI2C() inizializza il Bus I2C per spedire i messaggi - Usato dal Master
-  * Input:
-  *   - L'indirizzo del Modulo sul Bus i2c
-  * Restituisce:
-  *   - Nulla
-  */
-void initSusiOverI2C(uint8_t I2cAddr);
 	/*
 	*	initSusiOverI2C() inizializza il Bus I2C per spedire i messaggi - Usato dagli Slave
 	*	Input:
@@ -41,31 +30,5 @@ void initSusiOverI2C(uint8_t I2cAddr, Rcn600* SUSI);
 	*		- Nulla
 	*/
 void endSusiOverI2C(void);
-	/*
-	*	SearchExternalI2CDevices() controlla se sul Bus sono presenti dispositivi e ne memorizza l'indirizzo in una variabile privata
-	*	Input:
-	*		- Nulla
-	*	Restituisce:
-	*		- Il numero di dispositivi trovati
-	*/
-uint8_t SearchExternalI2CDevices(void);
-  /*
-  * sendByte() invia 1 Byte a tutti i dispositivi I2c collegati
-  * Input:
-  *   - il Byte da inviare
-  * Restituisce:
-  *   - Nulla
-  */
-void sendByte(uint8_t Byte);
-	/*
-	*	sendSusiMessage() invia, tramite Struct dedicata, tutti gli elementi di un messaggio Rcn600 Susi
-	*	Input:
-	*		- il primo Byte del messaggio
-	*		- il Secondo Byte del messaggio
-	*		- l'Argomento di manipolazione delle CVs
-	*	Restituisce:
-	*		- Nulla
-	*/
-void sendSusiMessage(uint8_t firstByte, uint8_t secondByte, uint8_t CvManipulating = 0);
 
 #endif
