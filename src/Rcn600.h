c8efe7478fce8b5bcfdae5928cee67d56f9ae523
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-10-27 14:30:34 +0200

    Update Rcn600.h

--- src/Rcn600.h
+++ src/Rcn600.h
@@ -7,10 +7,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#ifdef __AVR__	// Se la piattaforma e' AVR posso usare la libreria digitalPinFast; e' escludibile per risparmiare RAM
-	#include <util/delay.h>
+#ifdef __AVR__																															// Se la piattaforma e' AVR puo' essere usata la libreria digitalPinFast; e' escludibile per risparmiare SRAM
+	#include <util/delay.h>																												// Libreria per la gestione del Delay tramite libreria AVR
 
-	#define	DIGITAL_PIN_FAST
+	#define	DIGITAL_PIN_FAST																											// Libreria per la gestione 'fast' dei pin digitali, ESCLUDIBILE per risparmiare SRAM
 #endif // __AVR__
 
 #ifdef DIGITAL_PIN_FAST
