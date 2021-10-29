3da66ecf5b04dd084695c1afda708c947214ef9f
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-10-29 21:30:05 +0200

    Update SUSI_DATA_TYPE.h

--- src/DataHeaders/SUSI_DATA_TYPE.h
+++ src/DataHeaders/SUSI_DATA_TYPE.h
@@ -12,35 +12,35 @@
 
 /* Identificativi dei Gruppi Funzioni */
 #define	SUSI_FN_GROUP		uint8_t
-#define	SUSI_FN_0_4			1
-#define	SUSI_FN_5_12		2
-#define	SUSI_FN_13_20		3
-#define	SUSI_FN_21_28		4
-#define	SUSI_FN_29_36		5
-#define	SUSI_FN_37_44		6
-#define	SUSI_FN_45_52		7
-#define	SUSI_FN_53_60		8
-#define	SUSI_FN_61_68		9
+#define	SUSI_FN_0_4			0
+#define	SUSI_FN_5_12		1
+#define	SUSI_FN_13_20		2
+#define	SUSI_FN_21_28		3
+#define	SUSI_FN_29_36		4
+#define	SUSI_FN_37_44		5
+#define	SUSI_FN_45_52		6
+#define	SUSI_FN_53_60		7
+#define	SUSI_FN_61_68		8
 
 
 /* Identificativi dei Gruppi AUXs */
 #define	SUSI_AUX_GROUP		uint8_t
-#define	SUSI_AUX_1_8		1
-#define	SUSI_AUX_9_16		2
-#define	SUSI_AUX_17_24		3
-#define	SUSI_AUX_25_32		4
+#define	SUSI_AUX_1_8		0
+#define	SUSI_AUX_9_16		1
+#define	SUSI_AUX_17_24		2
+#define	SUSI_AUX_25_32		3
 
 
 /* Identificativi dei Gruppi Funzioni Analogiche */
 #define	SUSI_AN_GROUP		uint8_t
-#define	SUSI_AN_FN_0_7		1
-#define	SUSI_AN_FN_8_15		2
-#define	SUSI_AN_FN_16_23	3
-#define	SUSI_AN_FN_24_31	4
-#define	SUSI_AN_FN_32_39	5
-#define	SUSI_AN_FN_40_47	6
-#define	SUSI_AN_FN_48_55	7
-#define	SUSI_AN_FN_56_63	8
+#define	SUSI_AN_FN_0_7		0
+#define	SUSI_AN_FN_8_15		1
+#define	SUSI_AN_FN_16_23	2
+#define	SUSI_AN_FN_24_31	3
+#define	SUSI_AN_FN_32_39	4
+#define	SUSI_AN_FN_40_47	5
+#define	SUSI_AN_FN_48_55	6
+#define	SUSI_AN_FN_56_63	7
 
 
 #endif
