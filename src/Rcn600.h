e389f53fb5e9aec8171d3e36b0ebff79ae4ff382
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-12-20 09:27:17 +0100

    Update Rcn600.h

--- src/Rcn600.h
+++ src/Rcn600.h
@@ -75,19 +75,19 @@ typedef struct messageRcn600 {
 
 class Rcn600 {
     private:
-        uint8_t	_slaveAddress;										// identifica il numero dello slave sul Bus SUSI (valori da 1 a 3)
-        uint8_t	_CLK_pin;											// pin a cui e' collegata la linea "Clock", se il clock e' gestito dalla libreria DEVE ESSERE DI TIPO INTERRUPT 
+        uint8_t	_slaveAddress;                                                                                              // identifica il numero dello slave sul Bus SUSI (valori da 1 a 3)
+        uint8_t	_CLK_pin;                                                                                                   // pin a cui e' collegata la linea "Clock", se il clock e' gestito dalla libreria DEVE ESSERE DI TIPO INTERRUPT 
 
-        Rcn600Message _Buffer[SUSI_BUFFER_LENGTH];					// Buffer contenenti i comandi SUSI ricevuti
-        Rcn600Message* _BufferPointer;								// Puntatore per scorrere il Buffer
+        Rcn600Message _Buffer[SUSI_BUFFER_LENGTH];                                                                          // Buffer contenenti i comandi SUSI ricevuti
+        Rcn600Message* _BufferPointer;                                                                                      // Puntatore per scorrere il Buffer
 
 #ifdef	DIGITAL_PIN_FAST
-        digitalPinFast *_DATA_pin;									// Oggetto che contiene i dati del pin a cui e' collegata la linea Data
+        digitalPinFast *_DATA_pin                                                                                           // Oggetto che contiene i dati del pin a cui e' collegata la linea Data
 #else
-        uint8_t	_DATA_pin;											// pin a cui e' collegata la linea "Data", puo' essere un pin qualsiasi (Compresi gli analogici)
+        uint8_t _DATA_pin;                                                                                                  // pin a cui e' collegata la linea "Data", puo' essere un pin qualsiasi (Compresi gli analogici)
 #endif
 
-    private:                                                                                            /* Metodi Privati */
+    private:
         /*
         *   initClass() Inizializza la Classe e imposta a 'INPUT' i pin a cui e' connesso il Bus Rcn600
         *   Input:
@@ -107,9 +107,24 @@ class Rcn600 {
         void setNextMessage(Rcn600Message* nextMessage);			// Inserisce nel buffer un messaggio ricevuto Completo dall'ISR
         void processCVsMessage(Rcn600Message CvMessage);			// elabora Immediatamente un Messaggio che richiede un'interazione con le CVs
 
-    public:                                                                                             /* Metodi Pubblici */
-        Rcn600(uint8_t CLK_pin, uint8_t DATA_pin);					// Creazione dell'oggetto Rcn600 con pin di Interrupt e pin Data
-        ~Rcn600(void);												// Distruzionde dell'oggetto Rcn600
+    public:
+        /*
+        *   ~Rcn600() Creazione della Classe
+        *   Input:
+        *       - il Pin a cui e' connesso la linea 'Clock'
+        *       - il Pin a cui e' connessa la linea 'Data'
+        *   Restituisce:
+        *       - Nulla
+        */
+        Rcn600(uint8_t CLK_pin, uint8_t DATA_pin);
+        /*
+        *   ~Rcn600() Distruttore Classe
+        *   Input:
+        *       - Nulla
+        *   Restituisce:
+        *       - Nulla
+        */
+        ~Rcn600(void);
 
         /*
         *	init() Inizializza la libreria, utilizzando i metodi notifySusiCVread
@@ -151,156 +166,157 @@ class Rcn600 {
 #if defined (__cplusplus)
 extern "C" {
 #endif
-    /*
-    *	notifySusiRawMessage() viene invocato ogni volta che è presente un messaggio (2 Byte) da decodificare. NON viene invocato per i Messaggi di Manipolazione CVs. Mostra il messaggio Grezzo: NON DECODIFICATO.
-    *	Input:
-    *		- il Primo Byte del Messaggio
-    *		- il Secondo Byte del Messaggio
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiRawMessage(uint8_t firstByte, uint8_t secondByte) __attribute__((weak));
-    /*
-    *	notifySusiFunc() viene invocato quando: si ricevono i dati dal Master su un gruppo di funzioni digitali
-    *	Input:
-    *		- il gruppo Funzioni decodificato
-    *		- lo stato del gruppo funzioni
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) __attribute__((weak));
-    /*
-    *	notifySusiBinaryState() viene invocato quando: si ricevono i dati dal Master sullo stato di UNA specifica funzione
-    *	Input:
-    *		- il numero della funzione (da 1 a 127)
-    *		- lo stato della Funzione (attiva = 1, disattiva = 0)
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiBinaryState(uint16_t Command, uint8_t CommandState) __attribute__((weak));
-    /*
-    *	notifySusiAux() viene invocato quando: si ricevono i dati dal Master sullo stato di UNA specifica AUX
-    *	Input:
-    *		- il numero dell'AUX
-    *		- lo stato dell'uscita (attiva = 1, disattiva = 0)
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiAux(SUSI_AUX_GROUP SUSI_auxGrp, uint8_t SUSI_AuxState) __attribute__((weak));
-    /*
-    *	notifySusiTriggerPulse() viene invocato quando: si riceve dal Master il comando di Trigger (o pulsazione) per eventuali sbuffi di vapore
-    *	Input:
-    *		- stato del comando Trigger/Pulse
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiTriggerPulse(uint8_t state) __attribute__((weak));
-    /*
-    *	notifySusiMotorCurrent() viene invocato quando: si riceve dal Master i dati sull'assorbimento di Corrente da parte del Motore
-    *	Input:
-    *		- Assorbimento di Corrente: da -128 a + 127 (gia' convertita dal Complemento a 2 originale)
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiMotorCurrent(int8_t current) __attribute__((weak));
-    /*
-    *	notifySusiRequestSpeed() viene invocato quando: si ricevono i dati sulla Velocita' e sulla Direzione richiesti dalla Centrale al Master
-    *	Input:
-    *		- la velocita' (128 step) richiesta
-    *		- la direzione richiesta
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiRequestSpeed(uint8_t Speed, SUSI_DIRECTION Dir) __attribute__ ((weak));
-    /*
-    *	notifySusiRealSpeed() viene invocato quando: si ricevono i dati dal Master sulla Velocita' e sulla Direzione reali 
-    *	Input:
-    *		- la velocita' (128 step) reale
-    *		- la direzione reale
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiRealSpeed(uint8_t Speed, SUSI_DIRECTION Dir) __attribute__ ((weak));
-    /*
-    *	notifySusiMotorLoad() viene invocato quando: si riceve dal Master i dati sul carico del Motore
-    *	Input:
-    *		- Carico del Motore: da -128 a + 127 (gia' convertita dal Complemento a 2 originale)
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiMotorLoad(int8_t load) __attribute__((weak));
-    /*
-    *	notifySusiAnalogFunction() viene invocato quando: si ricevono i dati dal Master su un gruppo di funzioni analogiche
-    *	Input:
-    *		- il gruppo Analogico decodificato
-    *		- lo stato del gruppo
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiAnalogFunction(SUSI_AN_GROUP SUSI_AnalogGrp, uint8_t SUSI_AnalogState) __attribute__((weak));
-    /*
-    *	notifySusiAnalogDirectCommand() viene invocato quando: si ricevono i dati dal Master i comandi diretti per il funzionamento analogico
-    *	Input:
-    *		- il numero del comando: 1 o 2
-    *		- i bit del comando
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiAnalogDirectCommand(uint8_t commandNumber, uint8_t Command) __attribute__((weak));
-    /*
-    *	notifySusiNoOperation() viene invocato quando: si riceve il comando "no operation", serve prevalentemente a scopo di test
-    *	Input:
-    *		- l'argomento del comando
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiNoOperation(uint8_t commandArgument) __attribute__((weak));
-    /*
-    *	notifySusiMasterAddress() viene invocato quando: si riceve l'indirizzo digitale del Master
-    *	Input:
-    *		- l'indirizzo Digitale del Master
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiMasterAddress(uint16_t MasterAddress) __attribute__((weak));
-    /*
-    *	notifySusiControlModule() viene invocato quando: si riceve il comando sul controllo del modulo
-    *	Input:
-    *		- byte contenete il controllo del modulo
-    *	Restituisce:
-    *		- Nulla
-    */
-    extern	void notifySusiControllModule(uint8_t ModuleControll) __attribute__((weak));
+        /*
+        *	notifySusiRawMessage() viene invocato ogni volta che è presente un messaggio (2 Byte) da decodificare. NON viene invocato per i Messaggi di Manipolazione CVs. Mostra il messaggio Grezzo: NON DECODIFICATO.
+        *   Input:
+        *		- il Primo Byte del Messaggio
+        *		- il Secondo Byte del Messaggio
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiRawMessage(uint8_t firstByte, uint8_t secondByte) __attribute__((weak));
+        /*
+        *	notifySusiFunc() viene invocato quando: si ricevono i dati dal Master su un gruppo di funzioni digitali
+        *	Input:
+        *		- il gruppo Funzioni decodificato
+        *		- lo stato del gruppo funzioni
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) __attribute__((weak));
+        /*
+        *	notifySusiBinaryState() viene invocato quando: si ricevono i dati dal Master sullo stato di UNA specifica funzione
+        *	Input:
+        *		- il numero della funzione (da 1 a 127)
+        *		- lo stato della Funzione (attiva = 1, disattiva = 0)
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiBinaryState(uint16_t Command, uint8_t CommandState) __attribute__((weak));
+        /*
+        *	notifySusiAux() viene invocato quando: si ricevono i dati dal Master sullo stato di UNA specifica AUX
+        *	Input:
+        *		- il numero dell'AUX
+        *		- lo stato dell'uscita (attiva = 1, disattiva = 0)
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiAux(SUSI_AUX_GROUP SUSI_auxGrp, uint8_t SUSI_AuxState) __attribute__((weak));
+        /*
+        *	notifySusiTriggerPulse() viene invocato quando: si riceve dal Master il comando di Trigger (o pulsazione) per eventuali sbuffi di vapore
+        *	Input:
+        *		- stato del comando Trigger/Pulse
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiTriggerPulse(uint8_t state) __attribute__((weak));
+        /*
+        *	notifySusiMotorCurrent() viene invocato quando: si riceve dal Master i dati sull'assorbimento di Corrente da parte del Motore
+        *	Input:
+        *		- Assorbimento di Corrente: da -128 a + 127 (gia' convertita dal Complemento a 2 originale)
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiMotorCurrent(int8_t current) __attribute__((weak));
+        /*
+        *	notifySusiRequestSpeed() viene invocato quando: si ricevono i dati sulla Velocita' e sulla Direzione richiesti dalla Centrale al Master
+        *	Input:
+        *		- la velocita' (128 step) richiesta
+        *		- la direzione richiesta
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiRequestSpeed(uint8_t Speed, SUSI_DIRECTION Dir) __attribute__ ((weak));
+        /*
+        *	notifySusiRealSpeed() viene invocato quando: si ricevono i dati dal Master sulla Velocita' e sulla Direzione reali 
+        *	Input:
+        *		- la velocita' (128 step) reale
+        *		- la direzione reale
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiRealSpeed(uint8_t Speed, SUSI_DIRECTION Dir) __attribute__ ((weak));
+        /*
+        *	notifySusiMotorLoad() viene invocato quando: si riceve dal Master i dati sul carico del Motore
+        *	Input:
+        *		- Carico del Motore: da -128 a + 127 (gia' convertita dal Complemento a 2 originale)
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiMotorLoad(int8_t load) __attribute__((weak));
+        /*
+        *	notifySusiAnalogFunction() viene invocato quando: si ricevono i dati dal Master su un gruppo di funzioni analogiche
+        *	Input:
+        *		- il gruppo Analogico decodificato
+        *		- lo stato del gruppo
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiAnalogFunction(SUSI_AN_GROUP SUSI_AnalogGrp, uint8_t SUSI_AnalogState) __attribute__((weak));
+        /*
+        *	notifySusiAnalogDirectCommand() viene invocato quando: si ricevono i dati dal Master i comandi diretti per il funzionamento analogico
+        *	Input:
+        *		- il numero del comando: 1 o 2
+        *		- i bit del comando
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiAnalogDirectCommand(uint8_t commandNumber, uint8_t Command) __attribute__((weak));
+        /*
+        *	notifySusiNoOperation() viene invocato quando: si riceve il comando "no operation", serve prevalentemente a scopo di test
+        *	Input:
+        *		- l'argomento del comando
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiNoOperation(uint8_t commandArgument) __attribute__((weak));
+        /*
+        *	notifySusiMasterAddress() viene invocato quando: si riceve l'indirizzo digitale del Master
+        *	Input:
+        *		- l'indirizzo Digitale del Master
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiMasterAddress(uint16_t MasterAddress) __attribute__((weak));
+        /*
+        *	notifySusiControlModule() viene invocato quando: si riceve il comando sul controllo del modulo
+        *	Input:
+        *		- byte contenete il controllo del modulo
+        *	Restituisce:
+        *		- Nulla
+        */
+        extern	void notifySusiControllModule(uint8_t ModuleControll) __attribute__((weak));
 
     
 
-    /* METODI MANIPOLAZIONE CVs */
-    /*
-    *	notifySusiCVRead() viene invocato quando: e' richiesta la lettura di una CV
-    *	Input:
-    *		- il numero della CV da leggere
-    *	Restituisce:
-    *		- ritorna il valore della CV letta
-    */
-    extern uint8_t notifySusiCVRead(uint16_t CV) __attribute__((weak));
-    /*
-    *	notifySusiCVWrite() viene invocato quando: e' richiesta la Scrittura di una CV.
-    *	Input:
-    *		- il numero della CV richiesta
-    *		- il Nuovo valore della CV
-    *	Restituisce:
-    *		- il valore letto (post scrittura) nella posizione richiesta
-    */
-    extern uint8_t notifySusiCVWrite(uint16_t CV, uint8_t Value) __attribute__((weak));
-    /* RESET CVs, viene utilizzato lo stesso metodo della Libreria NmraDcc:
-    * 
-    *  notifyCVResetFactoryDefault() Called when CVs must be reset. This is called when CVs must be reset to their factory defaults.
-    *   Inputs:
-    *       - None                                                                                          
-    *   Returns:
-    *       - None
-    */
-    extern void notifyCVResetFactoryDefault(void) __attribute__((weak));
+        /* METODI MANIPOLAZIONE CVs */
+
+        /*
+        *	notifySusiCVRead() viene invocato quando: e' richiesta la lettura di una CV
+        *	Input:
+        *		- il numero della CV da leggere
+        *	Restituisce:
+        *		- ritorna il valore della CV letta
+        */
+        extern uint8_t notifySusiCVRead(uint16_t CV) __attribute__((weak));
+        /*
+        *	notifySusiCVWrite() viene invocato quando: e' richiesta la Scrittura di una CV.
+        *	Input:
+        *		- il numero della CV richiesta
+        *		- il Nuovo valore della CV
+        *	Restituisce:
+        *		- il valore letto (post scrittura) nella posizione richiesta
+        */
+        extern uint8_t notifySusiCVWrite(uint16_t CV, uint8_t Value) __attribute__((weak));
+        /* RESET CVs, viene utilizzato lo stesso metodo della Libreria NmraDcc:
+        * 
+        *  notifyCVResetFactoryDefault() Called when CVs must be reset. This is called when CVs must be reset to their factory defaults.
+        *   Inputs:
+        *       - None                                                                                          
+        *   Returns:
+        *       - None
+        */
+        extern void notifyCVResetFactoryDefault(void) __attribute__((weak));
 #if defined (__cplusplus)
 }
 #endif
